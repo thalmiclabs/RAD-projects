7d7af5a5d7c05d6f4b882e5aacab86c5adb46e89
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2015-05-01 19:46:50 -0400

    making the local server work with githubpages

--- sockpuppet/server.js
+++ sockpuppet/server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/../'));
 
-
-
+// This is for local testing only
 
 var port = 8000;
 app.listen(port);
